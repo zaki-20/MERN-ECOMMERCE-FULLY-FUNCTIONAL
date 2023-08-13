b828ac2cf922dff594ec56f2ee199ca8653f1d19
zaki-20 <zakigame199@gmail.com>
2023-08-13 10:47:14 +0500

    created token and saved in a cookie

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -2,6 +2,8 @@ const User = require("../models/userModel");
 const ErrorHandler = require("../utils/errorhandler");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 
+
+//create user with jwt
 exports.registerUser = catchAsyncErrors(async (req, res, next) => {
     const { name, email, password } = req.body
     const user = await User.create({
@@ -12,9 +14,39 @@ exports.registerUser = catchAsyncErrors(async (req, res, next) => {
         }
     })
 
+    const token = user.getJWTToken()
+
     res.status(201).json({
         success: true,
-        user
+        token
+    })
+
+})
+
+//login user 
+exports.loginUser = catchAsyncErrors(async (req, res, next) => {
+    const { email, password } = req.body;
+    //check if user gives emailand password both
+    if (!email || !password) {
+        next(new ErrorHandler("please enter username and password", 400))
+    }
+
+    const user = await User.findOne({email}).select("+password")
+    if(!user){
+        next(new ErrorHandler("invalid email or password", 401))
+    }
+
+    const isPasswordMatch = await user.comparePassword(password)
+
+    if(!isPasswordMatch){
+        next(new ErrorHandler("invalid email or password", 401))
+    }
+
+    const token = user.getJWTToken()
+
+    res.status(200).json({
+        success: true,
+        token
     })
 
 })
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
-// const jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 // const crypto = require("crypto");
 
 const userSchema = new mongoose.Schema({
@@ -54,5 +54,17 @@ userSchema.pre("save", async function(next){
    this.password = await bcrypt.hash(this.password, 10)
 })
 
+//generate Token for authentication 
+userSchema.methods.getJWTToken =  function(){
+    return jwt.sign({id:this.id}, process.env.JWT_SECRET,{
+        expiresIn: process.env.JWT_EXPIRE
+    })
+}
+
+//compare password
+userSchema.methods.comparePassword = async function(enteredPassword){
+    return await bcrypt.compare( enteredPassword, this.password)
+
+}
 
 module.exports = mongoose.model("User", userSchema);
