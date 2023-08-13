c5c4939e1013bcf794434da241bef25fbe83eb02
zaki-20 <zakigame199@gmail.com>
2023-08-13 23:26:46 +0500

    sending reset token to user mail using -nodemailer

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,7 +1,8 @@
 const User = require("../models/userModel");
 const ErrorHandler = require("../utils/errorhandler");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
-const sendToken = require("../utils/jwtToken")
+const sendToken = require("../utils/jwtToken");
+const sendEmail = require("../utils/sendEmail");
 
 
 //create user with jwt
@@ -30,18 +31,18 @@ exports.loginUser = catchAsyncErrors(async (req, res, next) => {
     const { email, password } = req.body;
     //check if user gives emailand password both
     if (!email || !password) {
-        next(new ErrorHandler("please enter username and password", 400))
+        return next(new ErrorHandler("please enter username and password", 400))
     }
 
-    const user = await User.findOne({email}).select("+password")
-    if(!user){
-        next(new ErrorHandler("invalid email or password", 401))
+    const user = await User.findOne({ email }).select("+password")
+    if (!user) {
+        return next(new ErrorHandler("invalid email or password", 401))
     }
 
     const isPasswordMatch = await user.comparePassword(password)
 
-    if(!isPasswordMatch){
-        next(new ErrorHandler("invalid email or password", 401))
+    if (!isPasswordMatch) {
+        return next(new ErrorHandler("invalid email or password", 401))
     }
 
     // const token = user.getJWTToken()
@@ -58,12 +59,50 @@ exports.loginUser = catchAsyncErrors(async (req, res, next) => {
 // Logout User
 exports.logout = catchAsyncErrors(async (req, res, next) => {
     res.cookie("token", null, {
-      expires: new Date(Date.now()),
-      httpOnly: true,
+        expires: new Date(Date.now()),
+        httpOnly: true,
     });
-  
+
     res.status(200).json({
-      success: true,
-      message: "Logged Out",
+        success: true,
+        message: "Logged Out",
     });
-  });
+});
+
+//forgot password
+exports.forgotPassword = catchAsyncErrors(async (req, res, next) => {
+    const user = await User.findOne({ email: req.body.email })
+
+    //check user exist
+    if (!user) {
+        return next(new ErrorHandler("user not found", 404))
+    }
+    const resetToken = user.getResetPasswordToken()
+
+    await user.save({ validateBeforeSave: false })
+
+    const resetPasswordUrl = `${req.protocol}://${req.get("host")}/password/reset/${resetToken}`;
+
+    const message = `Your password reset token is :- \n\n ${resetPasswordUrl} \n\nIf you have not requested this email then, please ignore it.`;
+
+    try {
+        await sendEmail({
+          email: user.email,
+          subject: `Zack Ecommerce Password Recovery`,
+          message,
+        });
+    
+        res.status(200).json({
+          success: true,
+          message: `Email sent to ${user.email} successfully`,
+        });
+      } catch (error) {
+        user.resetPasswordToken = undefined;
+        user.resetPasswordExpire = undefined;
+    
+        await user.save({ validateBeforeSave: false });
+    
+        return next(new ErrorHandler(error.message, 500));
+      }
+
+})
