877fffebfd264c52963003774aad2c6353cf0a1a
zaki-20 <zakigame199@gmail.com>
2023-08-14 20:01:30 +0500

    update profile api

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { registerUser, loginUser, logout, forgotPassword, resetPassword, getUserDetails, updatePassword } = require("../controller/userController");
+const { registerUser, loginUser, logout, forgotPassword, resetPassword, getUserDetails, updatePassword, updateProfile } = require("../controller/userController");
 const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 
 const router = express.Router()
@@ -11,5 +11,6 @@ router.route("/password/reset/:token").put(resetPassword)
 router.route("/logout").get(logout)
 router.route("/me").get(isAuthenticatedUser, getUserDetails)
 router.route("/password/update").put(isAuthenticatedUser, updatePassword)
+router.route("/me/update").put(isAuthenticatedUser, updateProfile)
 
 module.exports = router
