f0716caa5b17c8687c2729d102d9cba9e7f8f5a5
Zakria <zakigame199@gmail.com>
2023-08-20 09:32:42 +0500

    create new order Api

--- backend/app.js
+++ backend/app.js
@@ -9,10 +9,12 @@ app.use(cookieParser())
 //route imports
 const productRoute = require('./routes/productRoute')
 const userRoute = require("./routes/userRoute")
+const orderRoute = require("./routes/orderRoute")
 
 
 app.use("/api/v1", productRoute)
 app.use("/api/v1", userRoute)
+app.use("/api/v1", orderRoute)
 
 //middleware for error
 app.use(errorMiddleware)
---
+++ backend/routes/orderRoute.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const {
+    newOrder,
+
+} = require("../controller/orderController");
+const router = express.Router();
+
+const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
+
+
+router.route("/order/new").post(isAuthenticatedUser, newOrder);
+
+
+module.exports = router;
