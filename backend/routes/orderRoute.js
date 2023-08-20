190f3ea5b7576d106eef10c67abfa1ee6ced4801
Zakria <zakigame199@gmail.com>
2023-08-20 17:03:46 +0500

    order Api's complete, started frontend

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const {
-    newOrder, myOrder, getSingleOrder,
+    newOrder, myOrder, getSingleOrder, getAllOrders, updateOrder, deleteOrder,
 
 } = require("../controller/orderController");
 const router = express.Router();
@@ -11,6 +11,14 @@ const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 router.route("/order/new").post(isAuthenticatedUser, newOrder);
 router.route("/order/:id").get(isAuthenticatedUser, getSingleOrder);
 router.route("/orders/me").get(isAuthenticatedUser, myOrder);
+router
+  .route("/admin/orders")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), getAllOrders);
+
+router
+  .route("/admin/order/:id")
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateOrder)
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteOrder);
 
 
 module.exports = router;
