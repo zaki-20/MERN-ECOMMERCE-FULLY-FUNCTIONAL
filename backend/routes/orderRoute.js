c9a6c308a39404a64122778c313e820e98db9c25
Zakria <zakigame199@gmail.com>
2023-08-20 13:24:13 +0500

    get my order, get single order Api

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const {
-    newOrder,
+    newOrder, myOrder, getSingleOrder,
 
 } = require("../controller/orderController");
 const router = express.Router();
@@ -9,6 +9,8 @@ const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 
 
 router.route("/order/new").post(isAuthenticatedUser, newOrder);
+router.route("/order/:id").get(isAuthenticatedUser, getSingleOrder);
+router.route("/orders/me").get(isAuthenticatedUser, myOrder);
 
 
 module.exports = router;
